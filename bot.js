0d690262ef4ade6fb93d3cebe5548381be77bf72
vitorhugo643 <42257427+vitorhugo643@users.noreply.github.com>
2018-09-21 17:43:19 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,10 +16,7 @@ bot.on('guildMemberAdd', member => {
 });
 bot.on('ready', () =>{
     let status = [
-        {name: 'Ajuda?│!ajuda', type: 'STREAMING', url: 'https://twitch.tv/srmisterii'},
-        {name: '😍Nighty » Community😍', type: 'LISTENING'},
-        {name: '😉Steam😉', type: 'PLAYING'},
-        {name: 'Sr.Misterii│YouTube', type: 'WATCHING'},
+        {name: 'Ajuda?│/ajuda', type: 'STREAMING', url: 'https://twitch.tv/srmisterii'},
       ];
       
       //STREAMING = Transmitindo
