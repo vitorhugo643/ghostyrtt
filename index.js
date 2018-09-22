0a834f4cab8dce45b43b76b1a5b7617470166b5a
vitorhugo643 <42257427+vitorhugo643@users.noreply.github.com>
2018-09-22 12:37:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js");
+const client = new Discord.Client(); 
+const config = require("./config.json"); 
 
- 
-client.on('ready', () => {
-  let status = [
-        {name: 'Ajuda?│/ajuda', type: 'STREAMING', url: 'https://twitch.tv/srmisterii'},
-        {name: 'Tuê', type: 'LISTENING'},
-        {name: 'test', type: 'PLAYING'},
-        {name: 'NitrooPVP│YouTube', type: 'WATCHING'},
+client.on('ready', () =>{
+    let status = [
+        {name: 'Ajuda?│!ajuda', type: 'STREAMING', url: 'https://twitch.tv/srmisterii'},
+        {name: 'heart_eyesNighty » Communityheart_eyes', type: 'LISTENING'},
+        {name: 'winkSteamwink', type: 'PLAYING'},
+        {name: 'Sr.Misterii│YouTube', type: 'WATCHING'},
       ];
       
       //STREAMING = Transmitindo
@@ -37,21 +37,5 @@ message.author.send('${member.author},\n \n \n \nOlá, aqui está os comandos\n 
         message.channel.send('Meu criador: NitrooPVP#4025.');
     }
 });
-client.on('message', message => {
-    let arraymsg = message.content.split(" ");
-let cmd = arraymsg[0].toLowerCase()
-let args = message.content.split(" ").slice(1)
-    if(cmd === '/ban'){
-        const args = message.content.split(" ").slice(1);
-        var razao = args.slice(1).join(" ")
-            var membro = message.mentions.members.first();
-            if(!message.member.hasPermissions("BAN_MEMBERS")) return message.reply("você não tem permissão de usar esse comando")
-            if(!membro) return message.reply("você não mencinou ninguém")
-            if(!membro.bannable) return message.reply("Você não pode banir essa pessoa")
-            if(razao.length < 1) return message.reply("Coloque um motivo!")
-            membro.ban()
-            message.channel.send(`O membro ${membro.user.username} foi banido do servidor.\nMotivo: ${razao}`)
-      }
-});
-    
-client.login(process.env.BOT_TOKEN);
+
+client.login(config.token);
