600a0cc89dd1306009e8e1047fd4df2a916d7ec7
vitorhugo643 <42257427+vitorhugo643@users.noreply.github.com>
2018-09-22 12:00:49 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
  
-bot.on('ready', () => {
+client.on('ready', () => {
     console.log('Logado');
 });
-bot.on('message', message => {
+client.on('message', message => {
     if (message.content.startsWith('/ajuda')){
         message.channel.send('Estou em desenvolvimento, então quando adicionar comandos, meu criador avisara.\nCriador: NitrooPVP#4025.');
     }
@@ -19,7 +19,7 @@ message.author.send(`${member.author},\n \n \n \nOlá, aqui está os comandos\n 
         message.channel.send('Meu criador: NitrooPVP#4025.');
     }
 });
-bot.on('message', message => {
+client.on('message', message => {
     let arraymsg = message.content.split(" ");
 let cmd = arraymsg[0].toLowerCase()
 let args = message.content.split(" ").slice(1)
@@ -36,5 +36,4 @@ let args = message.content.split(" ").slice(1)
       }
 });
     
-// THIS  MUST  BE  THIS  WAY
-bot.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
