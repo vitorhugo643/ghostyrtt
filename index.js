fb0cbff903aa1494fbf910cd9fbfc0c8a4906036
vitorhugo643 <42257427+vitorhugo643@users.noreply.github.com>
2018-09-22 17:00:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const config = require("./config.json");
 
 
 client.on("ready", () => {
-    client.user.setPresence({ game: { name: 'comando', type: 2, url: 'https://www.twitch.tv/ladonegro'} });
+    client.user.setPresence({ game: { name: 'comando', type: 2, } });
     //0 = Jogando
     //  1 = Transmitindo
     //  2 = Ouvindo
