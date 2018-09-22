c1d2fd8c474a1e47ba6b24ca5e10023c94df472e
vitorhugo643 <42257427+vitorhugo643@users.noreply.github.com>
2018-09-22 16:22:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -48,24 +48,67 @@ client.on("guildDelete", guild => {
   console.log(`O Nighty BOT foi removido do servidor: ${guild.name} (id: ${guild.id})`);
   client.user.setActivity(`Estou em ${client.guilds.size} servidores.`);
 });
-client.on('message', message => {
-    let arraymsg = message.content.split(" ");
-let cmd = arraymsg[0].toLowerCase()
-    if(comando === "mute"){
-if (message.member.hasPermissions('MANAGE_ROLES')) return message.channel.send('Você não tem permissão para executar este comando!')
-  if (!args[0]) return message.channel.send("Mencione o membro!")
-  var user = message.mentions.members.first()
-  var razao = args.slice(1).join(' ') 
-  if (!razao) razao = "sem motivo"
-  var muteRole = message.guild.roles.find("name", "Muted")
-  if(!muteRole) return message.channel.send("Não encontrei o cargo Muted.");
-  try {
-    user.addRole(muteRole)
-    message.channel.send(user.tag +" foi mutado por"  + razao + "!");
-  } catch (err) { 
-    message.channel.send("Eu não tenho as permissões necessárias para mutar um membro!");
+
+
+client.on("message", async message => {
+  let responseObject = {
+    "oi" : "Olá, como você está hoje?",
+    "bem" : "Que bom que você está bem :slight_smile:"
+  };
+  
+  if (responseObject[message.content]){
+    message.channel.send(responseObject[message.content]);
   }
-}});
+
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+    if(!message.content.startsWith(config.prefix)) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const comando = args.shift().toLowerCase();
+  
+  if(comando === "ping") {
+    const m = await message.channel.send("Ping?");
+    message.delete().catch(O_o=>{});
+    m.edit(`${message.member}, Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms.`);
+  }
+  if(comando === "apagar") {
+    if(!message.member.roles.some(r=>["++", "BOT"].includes(r.name)) )
+    return message.reply("Desculpe mais você não tem permissão para usar isto!");
+    const deleteCount = parseInt(args[0], 10);
+    message.delete().catch(O_o=>{});
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Coloque ou forneça um número entre 2 e 100 para remover as mensagens!");
+    
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Não foi possível deletar mensagens devido a: ${error}`));
+  }
+  if(comando === "aviso") { 
+    if(!message.member.roles.some(r=>["++", "BOT"].includes(r.name)) )
+    return message.reply("Desculpe mais você não tem permissão para usar isto!");
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{});
+    message.channel.send("@everyone\n\n"+ sayMessage);
+  }
+  if(comando === "urgente") { 
+    if(!message.member.roles.some(r=>["++", "BOT"].includes(r.name)) )
+    return message.reply("Desculpe mais você não tem permissão para usar isto!");
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{});
+    message.channel.send("@here\n\n"+ sayMessage);
+  }
+  if(comando === "ajuda") {
+    const deleteCount = parseInt(args[0], 10);
+    message.delete().catch(O_o=>{});
+    message.reply("\n\nComandos do BOT:\n\nAdministradores:\n- /apagar <2 a 100> - Apagar as mensagens do chat.\n- /aviso <mensagem> - Avisar o servidor do discord.\n\nMembros:\n- /ping - Para ver seu ping/ms.\n- /criador - Para ver quem e meu Pai/Criador.");
+  }
+  if(comando === "criador") {
+    const deleteCount = parseInt(args[0], 10);
+    message.delete().catch(O_o=>{});
+    message.reply("\n\nQuem e meu Pai/Criador\n\n» NitrooPVP\n» Canal: https://www.youtube.com/NitrooPVP");
+  }
+});
 
 
 client.login(config.token);
