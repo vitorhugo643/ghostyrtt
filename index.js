e34dc2fd76b444d812374216482e16325040d686
vitorhugo643 <42257427+vitorhugo643@users.noreply.github.com>
2018-09-23 14:03:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -94,8 +94,7 @@ client.on("message", async message => {
     
     await member.kick(reason)
       .catch(error => message.reply(`Desculpe ${message.author} não consegui expulsar o membro devido o: ${error}`));
-    message.reply(`${member.user.tag} foi kickado por ${message.author.tag}\nMotivo: ${reason}`);
-      message.channels.get('480917048860344321')
+    message.channels.get("493465759524323348").send(`${member.user.tag} foi expulso por ${message.author.tag}\nMotivo: ${reason}`);
   }
    if(comando === "ban") {
     if(!message.member.hasPermissions("MANAGE_ROLES"))
@@ -109,7 +108,7 @@ client.on("message", async message => {
     if(!reason) reason = "Nenhuma razão fornecida";
     await member.ban(reason)
       .catch(error => message.reply(`Desculpe ${message.author} não consegui banir o membro devido o : ${error}`));
-    message.channels.get('480917048860344321').send(`${member.user.tag} foi banido por ${message.author.tag}\nMotivo: ${reason}`);
+    message.channels.get("493465759524323348").send(`${member.user.tag} foi banido por ${message.author.tag}\nMotivo: ${reason}`);
   }
    if(comando === "say") { 
     const sayMessage = args.join(" ");
