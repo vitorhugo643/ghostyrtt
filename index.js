704d39a3c4f3ce9cbb17b0cb26c13ea584e0a6de
vitorhugo643 <42257427+vitorhugo643@users.noreply.github.com>
2018-09-24 12:50:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -214,30 +214,6 @@ const embed = new Discord.RichEmbed()
                         member.removeRole(role);
                     }, ms(time));
 }
-     if (comando === "serverinfo") {
-    let online = message.guild.members.filter(a => a.presence.status == "online").size;
-    let ocupado = message.guild.members.filter(a => a.presence.status == "dnd").size;
-    let ausente = message.guild.members.filter(a => a.presence.status == "idle").size;
-    let offline = message.guild.members.filter(a => a.presence.status == "offline").size;
-    let bot = message.guild.members.filter(a => a.user.bot).size;
-    let totalmembros = message.guild.memberCount;
-    let canaistexto = message.guild.channels.filter(a => a.type === "text").size;
-    let canaisvoz = message.guild.channels.filter(a => a.type === "voice").size;
-    let cargos = message.guild.roles.map(a => a).join(", ")
-        const embed = new Discord.RichEmbed()
-        .setTitle(`Informações do servidor ${message.guild.name}`)
-        .setColor("#FF0000")
-        .addField('Dono', `<@${message.guild.owner.id}>`)
-        .addField('Criado em:', moment(message.guild.createdAt).format('LLLL'))
-        .addField("ID", message.guild.id)
-        .addField(`Membros [${totalmembros}]`, `Online: ${online}\nAusente: ${ausente}\n Ocupado: ${ocupado}\n Offline: ${offline}\n Bots: ${bot}`)
-        .addField(`Canais [${canaistexto+canaisvoz}]`, `Texto: ${canaistexto}\n Voz: ${canaisvoz}`)
-        .addField(`Cargos [${message.guild.roles.size}]`, cargos)
-        .setThumbnail(message.guild.iconURL)
-        .setFooter(`Pedido por ${message.author.tag}`, message.author.avatarURL)
-        message.channel.send(embed)
-  }
- }
 });
   
 
