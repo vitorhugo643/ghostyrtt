8a6a678d26c72bcf2938df32a36d1489a5c58e52
vitorhugo643 <42257427+vitorhugo643@users.noreply.github.com>
2018-09-25 14:11:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client(); 
-const config = require("./config.json"); 
+const config = require("./config.json");
+const ms = require("ms");
  
 
 client.on('ready', () =>{
