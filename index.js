862d1884d263bbc84aaba627f2e079e6fdf4fd00
vitorhugo643 <42257427+vitorhugo643@users.noreply.github.com>
2018-09-25 22:04:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,36 @@ bot.on("guildMemberAdd", member => {
     member.addRole(member.guild.roles.find(r => r.name == "MEMBRO"));
 })
 
+bot.on('guildMemberAdd', member => { 
+    
+    let embed = new Discord.RichEmbed()
+  .addField("Clique no emoji abaixo para completar a verificação!", " OBRIGADO!")
+  .setColor('#FF0000')
+  .setAuthor(`COMMUNITY - Verificação`, 'https://cdn.discordapp.com/attachments/444957023130353674/462671084907528213/460264772869554176.gif')
+  .setTimestamp();
+  
+  
+  
+  member.guild.channels.get('494309260163940362').send(`** ** ` + `<@` + member.user.id  + `>`);
+  
+  member.guild.channels.get('494309260163940362').send(embed).then(cona=> {
+    cona.react('⭕')
+  })
+   
+  
+    })
+  
+  
+  
+  
+  bot.on('messageReactionAdd', (reaction, user) => {
+    if(reaction.emoji.name === "⭕" && user.id !== bot.user.id) {
+         reaction.remove(user)
+         bot.guilds.get("464207744291962925").members.get(user.id).addRole('474731010886467606')
+         bot.guilds.get("464207744291962925").members.get(user.id).removeRole('494308686232158209')
+         
+    }
+  })
 bot.on('ready', () => {
     bot.user.setPresence({ game: { name: `NADA`, type: 1, url: 'https://www.youtube.com/yRecky'} });
     console.log('Logado');
